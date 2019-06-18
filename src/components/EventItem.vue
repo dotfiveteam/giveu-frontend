b28306c202b2b4d20e95d7d62146122dd30c2cd0
Youngjun Choi <yjaquians@gmail.com>
2019-06-18 16:16:34 +0900

    read local json to cards

--- src/components/EventItem.vue
+++ src/components/EventItem.vue
@@ -7,7 +7,7 @@
             <h3>{{title}}</h3>
           </div>
         </v-card-title>
-        <p class="text-xs-right">{{event.date}} 까지</p>
+        <!-- <p class="text-xs-right">{{event.date}} 까지</p> -->
         <v-img v-bind:src="thumbnail"></v-img>
         <v-card-actions>
           <v-btn outline block v-on:click="GoToVideo">구독, 좋아요, 댓글달기 ㄱㄱ</v-btn>
@@ -23,14 +23,14 @@ export default {
   props: ["event"],
   data() {
     return {
-      title: "get title from id",
-      thumbnail: "https://img.youtube.com/vi/" + this.event.videoId + "/0.jpg"
+      title: this.event.title,
+      thumbnail: "https://img.youtube.com/vi/" + this.event.video_id + "/0.jpg",
+      link: "https://www.youtube.com/watch?v=" + this.event.video_id
     };
   },
   methods: {
     GoToVideo() {
-      window.location.href =
-        "https://www.youtube.com/watch?v=" + this.event.videoId;
+      window.location.href = this.link;
     }
   }
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <Events v-bind:events="events"/>
+    <Events v-bind:events="video_list_tmp['(MUTUBE)와꾸대장봉준']"/>
   </div>
 </template>
 
 <script>
 import Events from "../components/Events";
+import video_list_tmp from "../assets/video_list_tmp.json";
 
 export default {
   name: "Home",
@@ -14,26 +15,27 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 1,
-          date: "2019-07-02",
-          finished: false,
-          videoId: "2-HAkAmdOAA"
-        },
-        {
-          id: 2,
-          date: "2019-03-23",
-          finished: true,
-          videoId: "a2GujJZfXpg"
-        },
-        {
-          id: 3,
-          date: "2019-02-12",
-          finished: true,
-          videoId: "L1BDM1oBRJ8"
-        }
-      ]
+      video_list_tmp: video_list_tmp
+      // events: [
+      //   {
+      //     id: 1,
+      //     date: "2019-07-02",
+      //     finished: false,
+      //     videoId: "2-HAkAmdOAA"
+      //   },
+      //   {
+      //     id: 2,
+      //     date: "2019-03-23",
+      //     finished: true,
+      //     videoId: "a2GujJZfXpg"
+      //   },
+      //   {
+      //     id: 3,
+      //     date: "2019-02-12",
+      //     finished: true,
+      //     videoId: "L1BDM1oBRJ8"
+      //   }
+      // ]
     };
   }
 };
